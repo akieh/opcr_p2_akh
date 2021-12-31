7db2a880007e9b8cf6cc7c5f1a447f8e16dea286
AKH <ab.khirdine@outlook.fr>
2021-12-31 11:24:22 +0100

    feat : ajout extract URL de plusieurs livres
    J'ai ajouté dans le fichier extractOneBook la fonction
    getUrlCategoryBooks() qui retourne une liste des URL de tous les livres
    d'une categorie. Il manque encore la fonctionnalité de pagination.
    Il y a aussi la fonctionnalité createSoup() et loadBooksCSV().

--- extractOneBook.py
+++ extractOneBook.py
@@ -6,68 +6,85 @@ import csv
 ## objet à manier
 url_site = "http://books.toscrape.com/"
 url_book = "http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html"
+url_category = "http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
 reponse = requests.get(url_book)
 page = reponse.content
 soup = BeautifulSoup(page, "html.parser")
-## header pour le excel
-en_tete = ["Product Page URL", "UPC", "Title", "Price including tax", "Price excluding tax", "Number available",
-           "Product description",
-           ]
-## "Category", "Review_rating", "Image URL"] à ajouter après Product description
-info_book = []
-
-# récupération du tableau des infos du livre
-info_tableau = []
-product_table = soup.find("table", class_="table table-striped")
-product_table_headers = product_table.find_all("th")
-
-for header in product_table_headers:
-    if header.text == "UPC" \
-    or header.text == "Price (excl. tax)" \
-    or header.text == "Price (incl. tax)" \
-    or header.text == "Availability":
-        info_tableau.append(header.find_next('td').text)
 
 
-##récupération de la description du produit dans le tableau
-product_description = soup.find(id="product_description")
-description_livre = product_description.find("h2").find_next('p').text
+def getInfoBook (url):
+    info_book = []
+    # récupération du tableau des infos du livre
+    info_tableau = []
+    product_table = soup.find("table", class_="table table-striped")
+    product_table_headers = product_table.find_all("th")
+    for header in product_table_headers:
+        if header.text == "UPC" \
+        or header.text == "Price (excl. tax)" \
+        or header.text == "Price (incl. tax)" \
+        or header.text == "Availability":
+            info_tableau.append(header.find_next('td').text)
 
-#récupération titre du livre et son rating
-product_main = soup.find (class_="col-sm-6 product_main")
-titre_livre = product_main.find("h1").text
-rating_livre = product_main.find(class_="star-rating")
+    #récupération de la description du produit dans le tableau
+    product_description = soup.find(id="product_description")
+    description_livre = product_description.find("h2").find_next('p').text
 
-#print ("le rating livre bis: ", rating_livre)
+    #récupération titre du livre et son rating
+    product_main = soup.find (class_="col-sm-6 product_main")
+    titre_livre = product_main.find("h1").text
+    rating_livre = product_main.find(class_="star-rating")
+    print ("!!!!!!!!!!!" ,rating_livre)
+
+    #print ("le rating livre bis: ", rating_livre)
+
+    #récupération catégorie du livre
+    categorie_livre = ""
+    bandeau_livre = soup.find("ul", class_="breadcrumb")
+    for ele in bandeau_livre:
+        if '<li> <a href="../category/books/sequential-art_5/index.html">Sequential Art</a> </li>' in ele:
+            print ("OUI IL Y A ")
+        else:
+            print ("nan y'a pas")
+        print (ele)
+    print("LA CATEGORIE DU LIVRE ",categorie_livre)
 
 
-#récupération catégorie du livre
-categorie_livre = ""
-bandeau_livre = soup.find("ul", class_="breadcrumb")
-for ele in bandeau_livre:
-    if '<li> <a href="../category/books/sequential-art_5/index.html">Sequential Art</a> </li>' in ele:
-        print ("OUI IL Y A ")
+    #récupération de l'url de l'image
+    image_source = soup.find(class_="item active").img['src']
+    url_image = url_site + image_source[6:]
+    #print ("L'URL de l'image: ", url_image)
+
+    #Présentation données du livre
+    info_book.append(url_book) # URL du livre
+    info_book.append(info_tableau[0]) # UPC du livre
+    info_book.append(titre_livre) # titre du livre
+    info_book.append(info_tableau[1]) # Prix tax excl du livre
+    info_book.append(info_tableau[2]) # Prix tax incl du livre
+    info_book.append(info_tableau[3]) # quantité dispo du livre
+    info_book.append(description_livre) # description du livre
+
+    print("Les infos du livre dans la fonction: ", info_book)
+
+    return info_book
+
+def getUrlCategoryBooks (url):
+    ### 30/12/2021 18:14, il faut créer une fonction soup et changer le soup ici
+    if soup.find(class_="next"):
+        page_url = soup.find()
+        print("there is a next")
     else:
-        print ("nan ya pas !!!!!!!!!!!!!!!!")
-    print (ele)
-print("LA CATEGORIE DU LIVRE ",categorie_livre)
+        print("there is no next pages")
 
+    table_books = soup.find("ol", class_="row")
+    url_all_books_category = table_books.find_all(href=True)
+    url_all_books = []
+    for url in url_all_books_category:
+        url_all_books.append(url_site + "catalogue/" + url['href'][9:])
 
-#récupération de l'url de l'image
-image_source = soup.find(class_="item active").img['src']
-url_image = url_site + image_source[6:]
-#print ("L'URL de l'image: ", url_image)
-
-#Présentation données du livre
-info_book.append(url_book) # URL du livre
-info_book.append(info_tableau[0]) # UPC du livre
-info_book.append(titre_livre) # titre du livre
-info_book.append(info_tableau[1]) # Prix tax excl du livre
-info_book.append(info_tableau[2]) # Prix tax incl du livre
-info_book.append(info_tableau[3]) # quantité dispo du livre
-info_book.append(description_livre) # description du livre
-
-print ("Les infos du livre du livre: ", info_book)
+    url_all_books = list(dict.fromkeys(url_all_books))
+    for url_book in enumerate(url_all_books):
+        print(url_book)
+    print ("rien")
 
 #chargement des données dans un fichier
 """
@@ -78,8 +95,32 @@ print ("Les infos du livre du livre: ", info_book)
     writer.writerow(info_book)
     print ("Terminé.")
 """
-#book = pd.DataFrame([info_book], columns=en_tete)
-#print ("Chargement des données du livre dans un fichier en cours ...")
-#book.to_csv('resultatbook/book.csv', index=False, encoding='utf-8')
-#print ("Terminé.")
+
+getInfoBook(url_book)
+
+def loadBooksCSV (info_book):
+    with open('resultatbook/book.csv', 'w') as fichier_csv:
+        ## header pour le excel
+        en_tete = ["Product Page URL", "UPC", "Title", "Price including tax", "Price excluding tax", "Number available",
+                   "Product description",
+                   ]
+        ## "Category", "Review_rating", "Image URL"] à ajouter après Product description
+        writer = csv.writer(fichier_csv, delimiter=',')
+        writer.writerow(en_tete)
+        print("Chargement des données du livre dans un fichier en cours ...")
+        writer.writerow(info_book)
+        print("Terminé.")
+
+def createSoup (url):
+    reponse = requests.get(url)
+    page = reponse.content
+    soup = BeautifulSoup(page, "html.parser")
+    return soup
+
+
+
+
+
+
+
 
